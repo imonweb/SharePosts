04acde83e681f48d2fdb9d8e02a7ced875310c26
Imon Dela Rosa <contact@imonweb.co.uk>
2018-05-07 18:02:51 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,14 @@
 <?php
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'imon');
-define('DB_PASS', 'p@ssw0rd');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'shareposts');
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL Root
-define('URLROOT', 'http://localhost/php/udemy/traversy/SharePosts');
+define('URLROOT', 'http://localhost/SharePosts');
 
 // Site Name
 define('SITENAME', 'SharePosts');
